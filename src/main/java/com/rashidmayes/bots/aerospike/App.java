3ccd4a6a142d6f60cf6474fe71dbf974a9f2c27b
rashidmayes <info@brilliancemobility.com>
2016-12-01 17:59:35 -0500

    no message

--- src/main/java/com/rashidmayes/bots/aerospike/App.java
+++ src/main/java/com/rashidmayes/bots/aerospike/App.java
@@ -2,7 +2,6 @@ package com.rashidmayes.bots.aerospike;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
